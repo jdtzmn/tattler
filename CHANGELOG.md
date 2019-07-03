08a513e32d82555374feb2db09f99ee71bb20dd8
jdtzmn <jdtzmn@gmail.com>
2019-07-03 18:59:31 -0400

    v0.2.0 (#1)
    * Added tattler class as well as eslint
    
    * Added declaration files
    
    * Added dist/ to npm files
    
    * Increment version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,22 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.2.0] - 2019-07-03
+
+### Added
+
+- eslint and tsconfig.json
+- parcel packaging
+- tattler class
+- Declaration files for TypeScript
+- dist as a directory to be downloaded when installing the package
+
+### Changed
+
+- parcel now does not start up a server
+- dev script to also create declaration files on save
+
+
 ## [0.1.0] - 2019-07-02
 
 ### Added
---
+++ dist/tattler.d.ts
@@ -0,0 +1,14 @@
+declare module "tattler" {
+    type AnyPromiseFunction = (...args: any) => Promise<any>;
+    export default class Tattler {
+        private uncalledPromise;
+        private cb?;
+        private promiseFunction;
+        private tattle;
+        constructor(uncalledPromise: AnyPromiseFunction);
+        private createHandler;
+        readonly mimic: AnyPromiseFunction;
+        readonly wait: Promise<any>;
+        setArgs(...args: any[]): void;
+    }
+}
